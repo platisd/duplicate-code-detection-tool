c44d0ff1eaaf4c99fc8b8b3ef2a28fb2fb985813
platisd <platisd@users.noreply.github.com>
2021-02-09 22:18:48 +0100

    Make file extensions and project root directory removal configurable

--- duplicate_code_detection.py
+++ duplicate_code_detection.py
@@ -13,7 +13,7 @@ import json
 from nltk.tokenize import word_tokenize
 from collections import OrderedDict
 
-source_code_file_extensions = [".c", ".cpp", ".cc", ".java", ".py", ".cs"]
+source_code_file_extensions = ["h", "c", "cpp", "cc", "java", "py", "cs"]
 file_column_label = "File"
 similarity_column_label = "Similarity (%)"
 similarity_label_length = len(similarity_column_label)
@@ -30,14 +30,14 @@ class CliColors:
     UNDERLINE = '\033[4m'
 
 
-def get_all_source_code_from_directory(directory):
+def get_all_source_code_from_directory(directory, file_extensions):
     """ Get a list with all the source code files within the directory
     """
     source_code_files = list()
     for dirpath, _, filenames in os.walk(directory):
         for name in filenames:
             _, file_extension = os.path.splitext(name)
-            if file_extension in source_code_file_extensions:
+            if file_extension[1:] in file_extensions:
                 filename = os.path.join(dirpath, name)
                 source_code_files.append(filename)
 
@@ -63,6 +63,10 @@ def main():
     parser.add_argument("--ignore-directories", nargs="+", help="Directories to ignore.")
     parser.add_argument("--ignore-files", nargs="+", help="Files to ignore.")
     parser.add_argument("-j", "--json", type=bool, default=False, help="Print output as JSON.")
+    parser.add_argument("--project-root-dir", type=str,
+        help="The relative path to the project root directory to be removed when printing out results.")
+    parser.add_argument("--file-extensions", nargs="+", default=source_code_file_extensions,
+        help="File extensions to check for similarities.")
     args = parser.parse_args()
 
     # Determine which files to compare for similarities
@@ -73,9 +77,9 @@ def main():
             if not os.path.isdir(directory):
                 print("Path does not exist or is not a directory:", directory)
                 sys.exit(1)
-            source_code_files += get_all_source_code_from_directory(directory)
+            source_code_files += get_all_source_code_from_directory(directory, args.file_extensions)
         for directory in args.ignore_directories:
-            files_to_ignore += get_all_source_code_from_directory(directory)
+            files_to_ignore += get_all_source_code_from_directory(directory, args.file_extensions)
     else:
         if len(args.files) < 2:
             print("Too few files to compare, you need to supply at least 2")
@@ -88,6 +92,17 @@ def main():
 
     files_to_ignore += args.ignore_files if args.ignore_files else list()
     source_code_files = list(set(source_code_files) - set(files_to_ignore))
+    if len(source_code_files) < 2:
+        print("Not enough source code files found")
+        sys.exit(1)
+
+    # Get the project root directory path to remove when printing out the results
+    project_root_index = 0
+    if args.project_root_dir:
+        if not os.path.isdir(args.project_root_dir):
+            print("The project root directory does not exist or is not a directory:", args.project_root_dir)
+            sys.exit(1)
+        project_root_index = len(os.path.abspath(args.project_root_dir)) + 1  # Remove the first slash
 
     # Parse the contents of all the source files
     source_code = OrderedDict()
@@ -114,25 +129,27 @@ def main():
         query_doc_bow = dictionary.doc2bow(query_doc)
         query_doc_tf_idf = tf_idf[query_doc_bow]
 
+        short_source_file_path = source_file[project_root_index:]
         conditional_print("\n\n\n" + CliColors.HEADER +
-              "Code duplication probability for " + source_file + CliColors.ENDC, args.json)
+              "Code duplication probability for " + short_source_file_path + CliColors.ENDC, args.json)
         conditional_print("-" * (largest_string_length + similarity_label_length), args.json)
         conditional_print(CliColors.BOLD + "%s %s" %
               (file_column_label.center(largest_string_length), similarity_column_label) + CliColors.ENDC, args.json)
         conditional_print("-" * (largest_string_length + similarity_label_length), args.json)
 
-        code_similarity[source_file] = dict()
+        code_similarity[short_source_file_path] = dict()
         for similarity, source in zip(sims[query_doc_tf_idf], source_code):
             # Ignore similarities for the same file
             if source == source_file:
                 continue
             similarity_percentage = similarity * 100
-            code_similarity[source_file][source] = round(similarity_percentage, 2)
+            short_source_path = source[project_root_index:]
+            code_similarity[short_source_file_path][short_source_path] = round(similarity_percentage, 2)
             if similarity_percentage > args.threshold:
                 exit_code = 1
             color = CliColors.OKGREEN if similarity_percentage < 10 else (
                 CliColors.WARNING if similarity_percentage < 20 else CliColors.FAIL)
-            conditional_print("%s     " % (source.ljust(largest_string_length)) +
+            conditional_print("%s     " % (short_source_path.ljust(largest_string_length)) +
                   color + "%.2f" % (similarity_percentage) + CliColors.ENDC, args.json)
     if exit_code == 1: 
         conditional_print("Code duplication threshold exceeded. Please consult logs.", args.json)
