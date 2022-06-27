61e92da67aa5b474c0ee993d6caf5288d0a19d39
platisd <platisd@users.noreply.github.com>
2022-06-27 21:07:13 +0200

    Format python files

--- duplicate_code_detection.py
+++ duplicate_code_detection.py
@@ -1,9 +1,9 @@
-'''
+"""
 A simple Python3 tool to detect similarities between files within a repository.
 
 Document similarity code adapted from Jonathan Mugan's tutorial:
 https://www.oreilly.com/learning/how-do-i-compare-document-similarity-using-python
-'''
+"""
 import os
 import sys
 import argparse
@@ -30,19 +30,18 @@ class ReturnCode(Enum):
 
 
 class CliColors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
 
 
 def get_all_source_code_from_directory(directory, file_extensions):
-    """ Get a list with all the source code files within the directory
-    """
+    """Get a list with all the source code files within the directory"""
     source_code_files = list()
     for dirpath, _, filenames in os.walk(directory):
         for name in filenames:
@@ -58,6 +57,7 @@ def conditional_print(text, machine_friendly_output):
     if not machine_friendly_output:
         print(text)
 
+
 def remove_comments_and_docstrings(source_code: str) -> str:
     """Strip comments and docstrings from source code
 
@@ -72,7 +72,9 @@ def remove_comments_and_docstrings(source_code: str) -> str:
     """
     parsed = ast.parse(source_code)
     for node in ast.walk(parsed):
-        if not isinstance(node, (ast.FunctionDef, ast.ClassDef, ast.AsyncFunctionDef, ast.Module)):
+        if not isinstance(
+            node, (ast.FunctionDef, ast.ClassDef, ast.AsyncFunctionDef, ast.Module)
+        ):
             continue
 
         if not len(node.body):
@@ -81,49 +83,106 @@ def remove_comments_and_docstrings(source_code: str) -> str:
         if not isinstance(node.body[0], ast.Expr):
             continue
 
-        if not hasattr(node.body[0], 'value') or not isinstance(node.body[0].value, ast.Str):
+        if not hasattr(node.body[0], "value") or not isinstance(
+            node.body[0].value, ast.Str
+        ):
             continue
 
         node.body = node.body[1:]
 
-    source_code_clean  = astor.to_source(parsed)
+    source_code_clean = astor.to_source(parsed)
     return source_code_clean
 
 
 def main():
-    parser_description = CliColors.HEADER + CliColors.BOLD + \
-        "=== Duplicate Code Detection Tool ===" + CliColors.ENDC
+    parser_description = (
+        CliColors.HEADER
+        + CliColors.BOLD
+        + "=== Duplicate Code Detection Tool ==="
+        + CliColors.ENDC
+    )
     parser = argparse.ArgumentParser(description=parser_description)
-    parser.add_argument("-t", "--fail-threshold", type=int, default=100,
-                        help="The maximum allowed similarity before the script exits with an error.")
+    parser.add_argument(
+        "-t",
+        "--fail-threshold",
+        type=int,
+        default=100,
+        help="The maximum allowed similarity before the script exits with an error.",
+    )
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("-d", "--directories", nargs="+",
-                       help="Check for similarities between all files of the specified directories.")
-    group.add_argument('-f', "--files", nargs="+", help="Check for similarities between specified files. \
-                        The more files are supplied the more accurate are the results.")
-    parser.add_argument("--ignore-directories", nargs="+", default=list(),
-                        help="Directories to ignore.")
+    group.add_argument(
+        "-d",
+        "--directories",
+        nargs="+",
+        help="Check for similarities between all files of the specified directories.",
+    )
+    group.add_argument(
+        "-f",
+        "--files",
+        nargs="+",
+        help="Check for similarities between specified files. \
+                        The more files are supplied the more accurate are the results.",
+    )
+    parser.add_argument(
+        "--ignore-directories", nargs="+", default=list(), help="Directories to ignore."
+    )
     parser.add_argument("--ignore-files", nargs="+", help="Files to ignore.")
-    parser.add_argument("-j", "--json", type=bool,
-                        default=False, help="Print output as JSON.")
-    parser.add_argument("--project-root-dir", type=str, default=str(),
-                        help="The relative path to the project root directory to be removed when printing out results.")
-    parser.add_argument("--file-extensions", nargs="+", default=source_code_file_extensions,
-                        help="File extensions to check for similarities.")
-    parser.add_argument("--ignore-threshold", type=int, default=0,
-                        help="Don't print out similarity below the ignore threshold")
-    parser.add_argument("--only-code", action="store_true", help="Removes comments and docstrings from the source code before analysis")
+    parser.add_argument(
+        "-j", "--json", type=bool, default=False, help="Print output as JSON."
+    )
+    parser.add_argument(
+        "--project-root-dir",
+        type=str,
+        default=str(),
+        help="The relative path to the project root directory to be removed when printing out results.",
+    )
+    parser.add_argument(
+        "--file-extensions",
+        nargs="+",
+        default=source_code_file_extensions,
+        help="File extensions to check for similarities.",
+    )
+    parser.add_argument(
+        "--ignore-threshold",
+        type=int,
+        default=0,
+        help="Don't print out similarity below the ignore threshold",
+    )
+    parser.add_argument(
+        "--only-code",
+        action="store_true",
+        help="Removes comments and docstrings from the source code before analysis",
+    )
     args = parser.parse_args()
 
-    result = run(args.fail_threshold, args.directories, args.files, args.ignore_directories,
-                 args.ignore_files, args.json, args.project_root_dir, args.file_extensions,
-                 args.ignore_threshold, args.only_code)
+    result = run(
+        args.fail_threshold,
+        args.directories,
+        args.files,
+        args.ignore_directories,
+        args.ignore_files,
+        args.json,
+        args.project_root_dir,
+        args.file_extensions,
+        args.ignore_threshold,
+        args.only_code,
+    )
 
     return result
 
 
-def run(fail_threshold, directories, files, ignore_directories, ignore_files,
-        json_output, project_root_dir, file_extensions, ignore_threshold, only_code,):
+def run(
+    fail_threshold,
+    directories,
+    files,
+    ignore_directories,
+    ignore_files,
+    json_output,
+    project_root_dir,
+    file_extensions,
+    ignore_threshold,
+    only_code,
+):
     # Determine which files to compare for similarities
     source_code_files = list()
     files_to_ignore = list()
@@ -133,10 +192,12 @@ def run(fail_threshold, directories, files, ignore_directories, ignore_files,
                 print("Path does not exist or is not a directory:", directory)
                 return (ReturnCode.BAD_INPUT, {})
             source_code_files += get_all_source_code_from_directory(
-                directory, file_extensions)
+                directory, file_extensions
+            )
         for directory in ignore_directories:
             files_to_ignore += get_all_source_code_from_directory(
-                directory, file_extensions)
+                directory, file_extensions
+            )
     else:
         if len(files) < 2:
             print("Too few files to compare, you need to supply at least 2")
@@ -158,56 +219,74 @@ def run(fail_threshold, directories, files, ignore_directories, ignore_files,
     if project_root_dir:
         if not os.path.isdir(project_root_dir):
             print(
-                "The project root directory does not exist or is not a directory:", project_root_dir)
+                "The project root directory does not exist or is not a directory:",
+                project_root_dir,
+            )
             return (ReturnCode.BAD_INPUT, {})
         project_root_dir = os.path.abspath(project_root_dir)
-        project_root_dir = os.path.join(
-            project_root_dir, '')  # Add the trailing slash
+        project_root_dir = os.path.join(project_root_dir, "")  # Add the trailing slash
 
     # Find the largest string length to format the textual output
-    largest_string_length = len(max(source_code_files, key=len).replace(project_root_dir, ""))
+    largest_string_length = len(
+        max(source_code_files, key=len).replace(project_root_dir, "")
+    )
 
     # Parse the contents of all the source files
     source_code = OrderedDict()
     for source_code_file in source_code_files:
         try:
             # read file but also recover from encoding errors in source files
-            with open(source_code_file, 'r', errors='surrogateescape') as f:
+            with open(source_code_file, "r", errors="surrogateescape") as f:
                 # Store source code with the file path as the key
                 content = f.read()
-                if only_code and source_code_file.endswith('py'):
+                if only_code and source_code_file.endswith("py"):
                     content = remove_comments_and_docstrings(content)
                 source_code[source_code_file] = content
         except Exception as err:
-            print(f'ERROR: Failed to open file {source_code_file}, reason: {str(err)}')
+            print(f"ERROR: Failed to open file {source_code_file}, reason: {str(err)}")
 
     # Create a Similarity object of all the source code
-    gen_docs = [[word.lower() for word in word_tokenize(source_code[source_file])]
-                for source_file in source_code]
+    gen_docs = [
+        [word.lower() for word in word_tokenize(source_code[source_file])]
+        for source_file in source_code
+    ]
     dictionary = gensim.corpora.Dictionary(gen_docs)
     corpus = [dictionary.doc2bow(gen_doc) for gen_doc in gen_docs]
     tf_idf = gensim.models.TfidfModel(corpus)
-    sims = gensim.similarities.Similarity(tempfile.gettempdir() + os.sep, tf_idf[corpus],
-                                          num_features=len(dictionary))
+    sims = gensim.similarities.Similarity(
+        tempfile.gettempdir() + os.sep, tf_idf[corpus], num_features=len(dictionary)
+    )
 
     exit_code = ReturnCode.SUCCESS
     code_similarity = dict()
     for source_file in source_code:
         # Check for similarities
-        query_doc = [w.lower()
-                     for w in word_tokenize(source_code[source_file])]
+        query_doc = [w.lower() for w in word_tokenize(source_code[source_file])]
         query_doc_bow = dictionary.doc2bow(query_doc)
         query_doc_tf_idf = tf_idf[query_doc_bow]
 
-        short_source_file_path = source_file.replace(project_root_dir, '')
-        conditional_print("\n\n\n" + CliColors.HEADER +
-                          "Code duplication probability for " + short_source_file_path + CliColors.ENDC, json_output)
-        conditional_print("-" * (largest_string_length +
-                                 similarity_label_length), json_output)
-        conditional_print(CliColors.BOLD + "%s %s" %
-                          (file_column_label.center(largest_string_length), similarity_column_label) + CliColors.ENDC, json_output)
-        conditional_print("-" * (largest_string_length +
-                                 similarity_label_length), json_output)
+        short_source_file_path = source_file.replace(project_root_dir, "")
+        conditional_print(
+            "\n\n\n"
+            + CliColors.HEADER
+            + "Code duplication probability for "
+            + short_source_file_path
+            + CliColors.ENDC,
+            json_output,
+        )
+        conditional_print(
+            "-" * (largest_string_length + similarity_label_length), json_output
+        )
+        conditional_print(
+            CliColors.BOLD
+            + "%s %s"
+            % (file_column_label.center(largest_string_length), similarity_column_label)
+            + CliColors.ENDC,
+            json_output,
+        )
+        conditional_print(
+            "-" * (largest_string_length + similarity_label_length), json_output
+        )
 
         code_similarity[short_source_file_path] = dict()
         for similarity, source in zip(sims[query_doc_tf_idf], source_code):
@@ -218,18 +297,30 @@ def run(fail_threshold, directories, files, ignore_directories, ignore_files,
             # Ignore very low similarity
             if similarity_percentage < ignore_threshold:
                 continue
-            short_source_path = source.replace(project_root_dir, '')
+            short_source_path = source.replace(project_root_dir, "")
             code_similarity[short_source_file_path][short_source_path] = round(
-                similarity_percentage, 2)
+                similarity_percentage, 2
+            )
             if similarity_percentage > fail_threshold:
                 exit_code = ReturnCode.THRESHOLD_EXCEEDED
-            color = CliColors.OKGREEN if similarity_percentage < 10 else (
-                CliColors.WARNING if similarity_percentage < 20 else CliColors.FAIL)
-            conditional_print("%s     " % (short_source_path.ljust(largest_string_length)) +
-                              color + "%.2f" % (similarity_percentage) + CliColors.ENDC, json_output)
+            color = (
+                CliColors.OKGREEN
+                if similarity_percentage < 10
+                else (
+                    CliColors.WARNING if similarity_percentage < 20 else CliColors.FAIL
+                )
+            )
+            conditional_print(
+                "%s     " % (short_source_path.ljust(largest_string_length))
+                + color
+                + "%.2f" % (similarity_percentage)
+                + CliColors.ENDC,
+                json_output,
+            )
     if exit_code == ReturnCode.THRESHOLD_EXCEEDED:
         conditional_print(
-            "Code duplication threshold exceeded. Please consult logs.", json_output)
+            "Code duplication threshold exceeded. Please consult logs.", json_output
+        )
     if json_output:
         similarities_json = json.dumps(code_similarity, indent=4)
         print(similarities_json)
--- run_action.py
+++ run_action.py
@@ -12,14 +12,14 @@ WARNING_SUFFIX = " ⚠️"
 
 
 def make_markdown_table(array):
-    """ Input: Python list with rows of table as lists
-               First element as header. 
-        Output: String to put into a .md file 
+    """Input: Python list with rows of table as lists
+               First element as header.
+        Output: String to put into a .md file
 
-    Ex Input: 
+    Ex Input:
         [["Name", "Age", "Height"],
          ["Jake", 20, 5'10],
-         ["Mary", 21, 5'7]] 
+         ["Mary", 21, 5'7]]
 
      Adopted from: https://gist.github.com/m0neysha/219bad4b02d2008e0154
     """
@@ -30,7 +30,7 @@ def make_markdown_table(array):
         markdown += to_add
     markdown += "\n"
 
-    markdown += '|'
+    markdown += "|"
     for i in range(len(array[0])):
         markdown += str("-------------- | ")
     markdown += "\n"
@@ -50,7 +50,11 @@ def get_markdown_link(file, url):
 
 
 def get_warning(similarity, warn_threshold):
-    return str(similarity) if similarity < int(warn_threshold) else str(similarity) + WARNING_SUFFIX
+    return (
+        str(similarity)
+        if similarity < int(warn_threshold)
+        else str(similarity) + WARNING_SUFFIX
+    )
 
 
 def similarities_to_markdown(similarities, url_prefix, warn_threshold):
@@ -60,15 +64,18 @@ def similarities_to_markdown(similarities, url_prefix, warn_threshold):
         markdown += "### 📄 %s\n" % get_markdown_link(checked_file, url_prefix)
 
         table_header = ["File", "Similarity (%)"]
-        table_contents = [[get_markdown_link(f, url_prefix), get_warning(s, warn_threshold)]
-                          for (f, s) in similarities[checked_file].items()]
+        table_contents = [
+            [get_markdown_link(f, url_prefix), get_warning(s, warn_threshold)]
+            for (f, s) in similarities[checked_file].items()
+        ]
         # Sort table contents based on similarity
         table_contents.sort(
-            reverse=True, key=lambda row: float(row[1].replace(WARNING_SUFFIX, "")))
+            reverse=True, key=lambda row: float(row[1].replace(WARNING_SUFFIX, ""))
+        )
         entire_table = [[] for _ in range(len(table_contents) + 1)]
         entire_table[0] = table_header
         for i in range(1, len(table_contents) + 1):
-            entire_table[i] = table_contents[i-1]
+            entire_table[i] = table_contents[i - 1]
 
         markdown += make_markdown_table(entire_table)
         markdown += "</details>\n"
@@ -77,7 +84,7 @@ def similarities_to_markdown(similarities, url_prefix, warn_threshold):
 
 
 def split_and_trim(input_list):
-    return [token.strip() for token in input_list.split(',')]
+    return [token.strip() for token in input_list.split(",")]
 
 
 def to_absolute_path(paths):
@@ -86,25 +93,32 @@ def to_absolute_path(paths):
 
 def main():
     parser = argparse.ArgumentParser(
-        description="Duplicate code detection action runner")
-    parser.add_argument("--latest-head", type=str,
-                        default="master", help="The latest commit hash or branch")
-    parser.add_argument("--pull-request-id", type=str,
-                        required=True, help="The pull request id")
+        description="Duplicate code detection action runner"
+    )
+    parser.add_argument(
+        "--latest-head",
+        type=str,
+        default="master",
+        help="The latest commit hash or branch",
+    )
+    parser.add_argument(
+        "--pull-request-id", type=str, required=True, help="The pull request id"
+    )
     args = parser.parse_args()
 
-    fail_threshold = os.environ.get('INPUT_FAIL_ABOVE')
-    directories = os.environ.get('INPUT_DIRECTORIES')
-    ignore_directories = os.environ.get('INPUT_IGNORE_DIRECTORIES')
-    project_root_dir = os.environ.get('INPUT_PROJECT_ROOT_DIR')
-    file_extensions = os.environ.get('INPUT_FILE_EXTENSIONS')
-    ignore_threshold = os.environ.get('INPUT_IGNORE_BELOW')
-    only_code = os.environ.get('INPUT_ONLY_CODE')
+    fail_threshold = os.environ.get("INPUT_FAIL_ABOVE")
+    directories = os.environ.get("INPUT_DIRECTORIES")
+    ignore_directories = os.environ.get("INPUT_IGNORE_DIRECTORIES")
+    project_root_dir = os.environ.get("INPUT_PROJECT_ROOT_DIR")
+    file_extensions = os.environ.get("INPUT_FILE_EXTENSIONS")
+    ignore_threshold = os.environ.get("INPUT_IGNORE_BELOW")
+    only_code = os.environ.get("INPUT_ONLY_CODE")
 
     directories_list = split_and_trim(directories)
     directories_list = to_absolute_path(directories_list)
-    ignore_directories_list = split_and_trim(
-        ignore_directories) if ignore_directories != '' else list()
+    ignore_directories_list = (
+        split_and_trim(ignore_directories) if ignore_directories != "" else list()
+    )
     ignore_directories_list = to_absolute_path(ignore_directories_list)
     file_extensions_list = split_and_trim(file_extensions)
     project_root_dir = os.path.abspath(project_root_dir)
@@ -113,41 +127,58 @@ def main():
     ignore_files_list = None
     json_output = True
 
-    detection_result, code_similarity = duplicate_code_detection.run(int(fail_threshold), directories_list, files_list,
-                                                                     ignore_directories_list, ignore_files_list,
-                                                                     json_output, project_root_dir, file_extensions_list,
-                                                                     int(ignore_threshold), bool(only_code))
+    detection_result, code_similarity = duplicate_code_detection.run(
+        int(fail_threshold),
+        directories_list,
+        files_list,
+        ignore_directories_list,
+        ignore_files_list,
+        json_output,
+        project_root_dir,
+        file_extensions_list,
+        int(ignore_threshold),
+        bool(only_code),
+    )
 
     if detection_result == duplicate_code_detection.ReturnCode.BAD_INPUT:
         print("Action aborted due to bad user input")
         return detection_result.value
     elif detection_result == duplicate_code_detection.ReturnCode.THRESHOLD_EXCEEDED:
         print(
-            "Action failed due to maximum similarity threshold exceeded, check the report")
+            "Action failed due to maximum similarity threshold exceeded, check the report"
+        )
 
-    repo = os.environ.get('GITHUB_REPOSITORY')
-    files_url_prefix = 'https://github.com/%s/blob/%s/' % (
-        repo, args.latest_head)
-    warn_threshold = os.environ.get('INPUT_WARN_ABOVE')
+    repo = os.environ.get("GITHUB_REPOSITORY")
+    files_url_prefix = "https://github.com/%s/blob/%s/" % (repo, args.latest_head)
+    warn_threshold = os.environ.get("INPUT_WARN_ABOVE")
 
     message = "## 📌 Duplicate code detection tool report\n"
     message += "The [tool](https://github.com/platisd/duplicate-code-detection-tool)"
     message += " analyzed your source code and found the following degree of"
     message += " similarity between the files:\n"
-    message += similarities_to_markdown(code_similarity,
-                                        files_url_prefix, warn_threshold)
+    message += similarities_to_markdown(
+        code_similarity, files_url_prefix, warn_threshold
+    )
 
-    github_token = os.environ.get('INPUT_GITHUB_TOKEN')
-    github_api_url = os.environ.get('GITHUB_API_URL')
-    request_url = '%s/repos/%s/issues/%s/comments' % (
-        github_api_url, repo, args.pull_request_id)
+    github_token = os.environ.get("INPUT_GITHUB_TOKEN")
+    github_api_url = os.environ.get("GITHUB_API_URL")
+    request_url = "%s/repos/%s/issues/%s/comments" % (
+        github_api_url,
+        repo,
+        args.pull_request_id,
+    )
 
-    post_result = requests.post(request_url, json={'body': message}, headers={
-                                'Authorization': 'token %s' % github_token})
+    post_result = requests.post(
+        request_url,
+        json={"body": message},
+        headers={"Authorization": "token %s" % github_token},
+    )
 
     if post_result.status_code != 201:
-        print("Posting results to GitHub failed with code: " +
-              str(post_result.status_code))
+        print(
+            "Posting results to GitHub failed with code: "
+            + str(post_result.status_code)
+        )
         print(post_result.text)
 
     return detection_result.value
